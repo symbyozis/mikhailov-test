6665bd20301d6eb14b30be778fc6840ebcb22e42
r.c. <r.chaturov.bas@innopolis.ru>
2026-01-14 00:08:52 +0300

    build

--- src/components/features/GridBox.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
-    <slot></slot>
-  </div>
-</template>
--- src/components/widgets/Header.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <header class="flex justify-center h-24 p-4">
-    <img src="@/assets/images/logo.svg" alt="Logo">
-  </header>
-</template>
